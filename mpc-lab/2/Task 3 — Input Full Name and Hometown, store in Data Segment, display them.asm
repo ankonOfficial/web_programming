2c43fd9aa74696426d04b413287a67a64ba4e856
Ankon Das <121180528+ankonOfficial@users.noreply.github.com>
2026-04-18 17:08:41 +0600

    Update Task 3 — Input Full Name and Hometown, store in Data Segment, display them.asm

--- mpc-lab/2/Task 3 — Input Full Name and Hometown, store in Data Segment, display them.asm
+++ mpc-lab/2/Task 3 — Input Full Name and Hometown, store in Data Segment, display them.asm
@@ -1,54 +1,51 @@
 .data
-msg_name:     .asciz "Your full name: "
-msg_town:     .asciz "Your hometown: "
-msg_name2:     .asciz "You said that your full name is: "
-msg_town2:     .asciz "You said that your hometown is: "
-name:         .space 100
-hometown:     .space 100
+msg_name: .asciz "ENter your name:"
+msg_hometown: .asciz "eNTER YOUR HOMEWOWN:"
+msg_name2:  .asciz "You said your name is :"
+msg_hometown2:  .asciz "You said your hometown is: "
+name:  .space 100
+hometown: .space 100
 
 .text
 .globl main
 main:
-    # Ask for full name
-    la a0, msg_name
-    li a7, 4 # sys call is print_string
-    ecall
 
-    # Read full name
-    la a0, name
-    li a1, 100
-    li a7, 8 # syscall for reading string
-    ecall
+la a0 ,msg_name
+li a7, 4
+ecall
 
-    # Ask for hometown
-    la a0, msg_town
-    li a7, 4 #syscall fo print_string
-    ecall
+la a0 ,name
+li a1, 100
+li a7, 8
+ecall
 
-    # Read hometown
-    la a0, hometown
-    li a1, 100
-    li a7, 8 # syscall for read string
-    ecall
+la a0,msg_hometown
+li a7, 4
+ecall
 
-    # Print full name
-    la a0, msg_name2
-    li a7, 4
-    ecall
 
-    la a0, name
-    li a7, 4
-    ecall
+la a0,hometown
+li a1, 100
+li a7, 8
+ecall
 
-    # Print hometown
-    la a0, msg_town2
-    li a7, 4
-    ecall
-
-    la a0, hometown
-    li a7, 4
-    ecall
-
-    # Exit
-    li a7, 10
-    ecall
+ 
+ la a0, msg_name2
+ li a7 ,4
+ ecall
+ 
+  la a0 ,name
+  li a7, 4
+  ecall
+  
+  la a0,msg_hometown2
+  li a7,4
+  ecall
+  
+  la a0,hometown
+  li a7,4
+  ecall
+  
+  
+  li a7,10
+  ecall
